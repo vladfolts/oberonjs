887bf8ff1d3b3bd28b6d9ecc46645768ca19f5cc
Vladislav Folts <vladfolts@gmail.com>
2014-03-13 18:54:55 -0500

    Fix export for pointer types.

--- test/expected/export.js
+++ test/expected/export.js
@@ -38,6 +38,10 @@ var T1 = RTL$.extend({
 	init: function T1(){
 	}
 });
+var NotExportedRecord = RTL$.extend({
+	init: function NotExportedRecord(){
+	}
+});
 var pr1 = null;
 var p2 = null;
 var vi = 0;
@@ -47,6 +51,7 @@ function p1(){
 return {
 	ci: ci,
 	T1: T1,
+	PNotExportedRecord: NotExportedRecord,
 	pr1: function(){return pr1;},
 	p2: function(){return p2;},
 	vi: function(){return vi;},
