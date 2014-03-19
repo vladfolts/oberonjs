b0d6bbe1eb2e5245a20bd2cf9308e27743e6e5e7
Vladislav Folts <vladfolts@gmail.com>
2014-03-19 10:16:09 -0500

    .

--- build.py
+++ build.py
@@ -125,5 +125,9 @@ if __name__ == '__main__':
     parser.add_option('--no-git', help='do not pull from git', action="store_true")
     parser.add_option('--pack', help='pack compiled source', action="store_true")
     (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.print_help();
+        exit(-1)
+        
     options.output = args[0]
     build(options)
