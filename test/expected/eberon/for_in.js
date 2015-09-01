a1ad9037f045e3a1ecb2131c847394785a23b5fd
Vladislav Folts <vladfolts@gmail.com>
2015-08-31 20:15:50 -0500

    Utilize fixed FOR..IN

--- test/expected/eberon/for_in.js
+++ test/expected/eberon/for_in.js
@@ -4,7 +4,7 @@ var test = function (){
 function array(){
 	var a = RTL$.makeArray(3, false);
 	var $seq1 = a;
-	for(var i = 0; i < $seq1.length; ++i{
+	for(var i = 0; i < $seq1.length; ++i){
 		var v = $seq1[i];
 		RTL$.assert(a[i] == v);
 	}
@@ -13,7 +13,7 @@ function array(){
 function arrayWithValueOnly(){
 	var a = RTL$.makeArray(3, false);
 	var $seq1 = a;
-	for(var $key2 = 0; $key2 < $seq1.length; ++$key2{
+	for(var $key2 = 0; $key2 < $seq1.length; ++$key2){
 		var v = $seq1[$key2];
 		RTL$.assert(!v);
 	}
